0329e63d620e6bf4f837fff28fbb0d9999ed29d6
Zoe Sitbon <zoe.sitbon@gmail.com>
2026-03-04 14:26:18 +0100

    modification chat

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :chats, only: [:create, :show] do
+  resources :chats, only: [:index, :create, :show] do
     resources :messages, only: [:create]
+
+    member do
+      post :generate_itinerary
+    end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
